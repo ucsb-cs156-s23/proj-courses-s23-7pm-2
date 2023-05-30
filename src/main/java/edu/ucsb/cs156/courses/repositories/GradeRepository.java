ef63da33a2a06f6caa518986aa64d543b512135c
Maya Ma <yema@cs.ucsb.edu>
2023-05-30 15:08:58 -0700

    mm-addedGradeRepository

---
+++ src/main/java/edu/ucsb/cs156/courses/repositories/GradeRepository.java
@@ -0,0 +1,15 @@
+package edu.ucsb.cs156.courses.repositories;
+
+import edu.ucsb.cs156.courses.entities.Grade;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface GradeRepository extends CrudRepository<Grade, Long> {
+//   Optional<Grade> findbyCourseName(String courseName);
+//   Optional<Grade> findbyCourseNameAndQuarter(String courseName, String quarter);
+}
