6243046fa2d313a0fcf812ab2a64b88f1ba8396c
Nathan Hoang <114448980+NateHoang@users.noreply.github.com>
2023-06-01 13:59:55 -0700

    Added the component into the instructor page for the search bar

--- frontend/src/main/pages/SearchByInstructor/SearchByInstructorPage.js
+++ frontend/src/main/pages/SearchByInstructor/SearchByInstructorPage.js
@@ -1,16 +1,41 @@
-// import { useState } from "react";
+import { useState } from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-//import CourseByInstructorSearchForm from "main/components/BasicCourseSearch/CourseByInstructorSearchForm";
-// import CourseOverTimeSearchForm from "main/components/BasicCourseSearch/CourseOverTimeSearchForm";
-// import { useBackendMutation } from "main/utils/useBackend";
-// import SectionsOverTimeTable from "main/components/Sections/SectionsOverTimeTable";
+import CourseByInstructorSearchForm from "main/components/BasicCourseSearch/CourseByInstructorSearchForm";
+import BasicCourseTable from "main/components/Courses/BasicCourseTable";
+import { useBackendMutation } from "main/utils/useBackend";
 
-export default function SearchByInstructorPage() {
+export default function CourseDescriptionIndexPage() {
+  const [courseJSON, setCourseJSON] = useState([]);
+
+  const objectToAxiosParams = (query) => ({
+    url: "/api/public/basicsearch",
+    params: {
+      qtr: query.quarter,
+      dept: query.subject,
+      level: query.level,
+    },
+  });
+
+  const onSuccess = (courses) => {
+    setCourseJSON(courses.classes);
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    []
+  );
+
+  async function fetchBasicCourseJSON(_event, query) {
+    mutation.mutate(query);
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h5>Welcome to the UCSB Course Search by Instructor!</h5>
-        
+        <h5>Welcome to the UCSB Courses Description Search!</h5>
+        <CourseByInstructorSearchForm fetchJSON={fetchBasicCourseJSON} />
+        <BasicCourseTable courses={courseJSON} />
       </div>
     </BasicLayout>
   );
