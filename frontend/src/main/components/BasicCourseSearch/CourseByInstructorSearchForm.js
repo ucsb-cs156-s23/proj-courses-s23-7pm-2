e4bb3de6a8c499e02a4cbb9b6268f01ddb53ff72
Arik <arikcheslog@gmail.com>
2023-06-01 17:35:54 -0700

    ac - make checkbox use localstorage properly

--- frontend/src/main/components/BasicCourseSearch/CourseByInstructorSearchForm.js
+++ frontend/src/main/components/BasicCourseSearch/CourseByInstructorSearchForm.js
@@ -24,7 +24,7 @@ const CourseByInstructorSearchForm = ({ fetchJSON }) => {
   const quarters = quarterRange(quarterRangeFirst, quarterRangeLast);
 
   const localStorageInstructor = localStorage.getItem("CourseByInstructorSearch.Instructor");
-  const localStorageCheckbox = localStorage.getItem("CourseByInstructorSearch.Checkbox");
+  const localStorageCheckbox = localStorage.getItem("CourseByInstructorSearch.Checkbox") === "true";
 
   const initialInstructor = localStorageInstructor || "";
   const initialCheckbox = localStorageCheckbox || false;
