20e8c2a97cad0d00beba1628fc4abfee81887a8c
Arik <arikcheslog@gmail.com>
2023-06-02 21:33:28 -0700

    ac - attempt to fix CI

--- frontend/src/tests/pages/Instructor/InstructorSearchPage.test.js
+++ frontend/src/tests/pages/Instructor/InstructorSearchPage.test.js
@@ -1,9 +1,14 @@
 import { render} from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
 import InstructorSearchPage from "main/pages/Instructor/InstructorSearchPage";
 
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 
+//
 const mockToast = jest.fn();
 jest.mock('react-toastify', () => {
     const originalModule = jest.requireActual('react-toastify');
@@ -16,6 +21,10 @@ jest.mock('react-toastify', () => {
 
 describe("InstructorSearchPage tests", () => {
     const queryClient = new QueryClient();
+    const axiosMock = new AxiosMockAdapter(axios);
+    
+    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
     test("renders without crashing", () => {
         render(
         <QueryClientProvider client={queryClient}>
