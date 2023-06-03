93a9f3066c2d4701893dc475ed0654f151896f59
Justin Cao <justinthecao@gmail.com>
2023-06-02 19:28:51 -0700

    removing unneeded variables

--- frontend/src/tests/pages/Instructor/InstructorSearchPage.test.js
+++ frontend/src/tests/pages/Instructor/InstructorSearchPage.test.js
@@ -1,15 +1,7 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render} from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
-import axios from "axios";
-import AxiosMockAdapter from "axios-mock-adapter";
-
 import InstructorSearchPage from "main/pages/Instructor/InstructorSearchPage";
-import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
-import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import { threeSections } from "fixtures/sectionFixtures";
-import { allTheSubjects } from "fixtures/subjectFixtures";
-import userEvent from "@testing-library/user-event";
 
 
 const mockToast = jest.fn();
