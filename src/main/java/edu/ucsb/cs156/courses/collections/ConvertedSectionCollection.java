4b275d3538d62a7be790a1032b13c1a01975dfc9
Jackson Newman <jpnewman167@gmail.com>
2023-06-07 12:08:23 -0700

    merge main

--- src/main/java/edu/ucsb/cs156/courses/collections/ConvertedSectionCollection.java
+++ src/main/java/edu/ucsb/cs156/courses/collections/ConvertedSectionCollection.java
@@ -11,8 +11,8 @@ import edu.ucsb.cs156.courses.documents.ConvertedSection;
 
 @Repository
 public interface ConvertedSectionCollection extends MongoRepository<ConvertedSection, ObjectId> {
-        @Query("{'courseInfo.quarter': ?0, 'section.enrollCode': ?1}")
-        Optional<ConvertedSection> findOneByQuarterAndEnrollCode(String quarter, String enrollCode);
+    @Query("{'courseInfo.quarter': ?0, 'section.enrollCode': ?1}")
+    Optional<ConvertedSection> findOneByQuarterAndEnrollCode(String quarter, String enrollCode);
 
     @Query("{'courseInfo.quarter': {$gte: ?0, $lte: ?1}, 'courseInfo.courseId': { $regex: ?2 }}")
     List<ConvertedSection> findByQuarterRangeAndCourseId(
